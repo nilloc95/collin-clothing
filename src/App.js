fa8f96ec001f8ec520a2b5535702f6cb8b442dcc
Collin Gilmore <collingilmore@gmail.com>
2020-09-01 15:48:22 -0400

    added header and logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import HomePage from './Pages/Homepage/homepage';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './Pages/Shop/shop'
+import Header from './Components/header/header'
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
