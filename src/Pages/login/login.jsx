abf0165e25e91177f1c2daa8b8938997a52d59a2
Collin Gilmore <collingilmore@gmail.com>
2020-09-10 13:13:44 -0400

    implemented firebase for sign in and sign up using either google sign in or email and password

--- src/Pages/login/login.jsx
+++ src/Pages/login/login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './login.scss'
-import SignIn from '../../Components/sign-in/sign-in'
+import SignIn from '../../Components/sign-in/sign-in';
+import SignUp from '../../Components/sign-up/sign-up';
 
 const LogIn = () => (
-    <div className='sign-in-and-sign-up'>
+    <div className='login'>
         <SignIn/>
+        <SignUp/>
     </div>
 )
 
